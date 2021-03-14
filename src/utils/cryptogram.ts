3b4ac174d961746a465d5221291168ef0d7b317a
oloshe <jacob.j@qq.com>
2021-03-15 00:22:54 +0800

    tset

--- src/utils/cryptogram.ts
+++ src/utils/cryptogram.ts
@@ -4,7 +4,7 @@ import * as crypto from 'crypto';
  * Make salt
  */
 export function makeSalt(): string {
-  return crypto.randomBytes(3).toString('base64');
+  return crypto.randomBytes(3).toString('base64')
 }
 
 /**
@@ -16,7 +16,7 @@ export function encryptPassword(password: string, salt: string): string {
   if (!password || !salt) {
     return '';
   }
-  const tempSalt = Buffer.from(salt, 'base64');
+  const tempSalt = Buffer.from(salt, 'base64')
   return (
     // 10000 代表迭代次数 16代表长度
     crypto.pbkdf2Sync(password, tempSalt, 10000, 16, 'sha1').toString('base64')
