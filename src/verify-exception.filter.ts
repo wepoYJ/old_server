d1cfaee6353e163d74916f9ad205273c7e754900
oloshe <jacob.j@qq.com>
2021-03-14 18:33:36 +0800

    validation fomat

--- src/verify-exception.filter.ts
+++ src/verify-exception.filter.ts
@@ -6,11 +6,11 @@ export class VerifyExceptionFilter<T> implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     if (exception instanceof HttpException && exception.getStatus() === HttpStatus.BAD_REQUEST) {
-      let msg: string
-      try {
-        msg = (exception.getResponse() as any).message[0]
-      } catch {
-        msg = exception.message
+      let msg: string, message = (exception.getResponse() as any).message
+      if (Array.isArray(message)) {
+        msg = message[0]
+      } else {
+        msg = message
       }
       response.status(HttpStatus.BAD_REQUEST).json({
         code: HttpStatus.BAD_REQUEST,
